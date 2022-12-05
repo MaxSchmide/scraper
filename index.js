b64ffd600ca2a29e87e7ebd8405a64fa4e5e08e5
MaxSchmide <kuznetsov.max.v@gmail.com>
2022-12-05 03:17:08 +0200

    init

---
+++ index.js
@@ -0,0 +1,55 @@
+const axios = require("axios")
+const cheerio = require("cheerio")
+const express = require("express")
+
+const app = express()
+const PORT = 8000
+const baseURL = "https://madappgang.com/"
+
+let allEmails = []
+
+const request = async (url) => {
+	return await axios({
+		url: url,
+		baseURL: baseURL,
+		headers: {
+			"accept-encoding": null,
+		},
+	})
+}
+
+const sortEmails = () => {
+	return (allEmails = allEmails.filter((el, i, arr) => i === arr.indexOf(el)))
+}
+
+const scanPage = (url) => {
+	try {
+		return request(url).then((res) => {
+			const html = res.data
+			const $ = cheerio.load(html)
+			const links = []
+
+			$("a[href^=/]").each(function () {
+				let link = $(this).attr("href")
+				links.push(link)
+			})
+			$("a[href^=mailto:]").each(function () {
+				let email = $(this).attr("href").replace("mailto:", "")
+				allEmails.push(email)
+			})
+			sortEmails()
+			return { links }
+		})
+	} catch (err) {
+		console.log(err)
+	}
+}
+
+scanPage().then(({ links }) => {
+	links.map((link, i) => {
+		scanPage(link)
+	})
+	console.log(allEmails)
+})
+
+app.listen(PORT, () => console.log(`Server running on PORT ${PORT}`))
